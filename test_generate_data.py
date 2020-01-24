140f23c598977eeba14986f9b50e901fce405931
Juan Orduz <juanorduz@TD-Juans-MacBook-Pro.local>
2020-01-24 15:59:39 +0100

    add generate data func

--- test_generate_data.py
+++ test_generate_data.py
@@ -3,6 +3,6 @@ from generate_data import *
 import pandas as pd
 
 def test_generate_linear_data():
-    #generate_linear_data(n=100, betas=(1.0, 3.0), sigma=0.2)
+    generate_linear_data(n=100, betas=(1.0, 3.0), sigma=0.2)
     t = 1 + 1
     assert t==2
