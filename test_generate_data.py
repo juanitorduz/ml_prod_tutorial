a287b4b3bcd64d51f623f4dd5beb389518fa27a8
juanitorduz <juanitorduz@gmail.com>
2020-01-24 21:46:02 +0100

    add other test

--- test_generate_data.py
+++ test_generate_data.py
@@ -2,7 +2,15 @@ import pytest
 from generate_data import *
 import pandas as pd
 
-def test_generate_linear_data():
+
+def test_generate_linear_data_shape():
     generate_linear_data(n=100, betas=(1.0, 3.0), sigma=0.2)
     df = pd.read_csv('data/train_data.csv')
     assert df.shape == (100, 2)
+
+
+def test_generate_linear_colnames():
+    generate_linear_data(n=100, betas=(1.0, 3.0), sigma=0.2)
+    df = pd.read_csv('data/train_data.csv')
+    same_cols = df.columns == ['x', 'y']
+    assert same_cols.all()
